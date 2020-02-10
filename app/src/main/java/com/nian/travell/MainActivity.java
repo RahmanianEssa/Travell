04914963c66faddd849ff0ba60cfae1d9fff1526
RahmanianEssa <rahmanianessa1@gmail.com>
2020-02-10 10:32:31 +0700

    Initial commit

---
+++ app/src/main/java/com/nian/travell/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nian.travell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
